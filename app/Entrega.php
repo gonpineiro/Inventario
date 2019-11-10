22fdeeeb848ba6f252e4923ad6ef5a90ecea40c3
Gonzalo Piñeiro <gon.pineiro@gmail.com>
2019-11-10 02:10:46 -0300

    Tabla entregas

---
+++ app/Entrega.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Entrega extends Model
+{
+    protected $guarded = [];
+
+    public function host(){
+
+      return $this->belongsTo('App\Host');
+    }
+    public function user_host(){
+
+      return $this->belongsTo('App\User_host');
+    }
+    public function ficha_entrega(){
+
+      return $this->belongsTo('App\Fichas_entregas');
+    }
+}
