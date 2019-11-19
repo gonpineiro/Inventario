a162628497cd51ce541bcf3694230cecdc176a85
Gonzalo Piñeiro <gon.pineiro@gmail.com>
2019-11-19 19:32:36 -0300

    Agrego la logica de validacion en OptionSelect

--- resources/views/administracion/fichasentrega/add_fichaentrega.blade.php
+++ resources/views/administracion/fichasentrega/add_fichaentrega.blade.php
@@ -12,7 +12,8 @@
                   <div class="form-row">
                     <div class="form-group col-md-9">
                       <label for="user_host_id">Usuario</label>
-                      <select class="form-control" name="user_host_id">
+                      <select class="form-control" name="user_host_id">                        
+                        <option value="">- - - Seleccione - - -</option>
                         @foreach ($userhosts as $userhost)
                           <option value="{{$userhost->id}}">{{$userhost->name}} {{$userhost->apellido}} | {{$userhost->departament->name}} - {{$userhost->departament->cliente->name}}</option>
                         @endforeach
@@ -27,22 +28,24 @@
                   <table class="table table-hover" style="width:100%">
                     <thead>
                       <tr>
-                        <th scope="col" style="width:15%">Cant</th>
-                        <th scope="col"style="width:40%">Marca/Modelo/SN</th>
-                        <th scope="col"style="width:55%">Observaciones</th>
+                        <th scope="col" style="width:10%">Cant</th>
+                        <th scope="col"style="width:50%">Marca/Modelo/SN</th>
+                        <th scope="col"style="width:40%">Observaciones</th>
                       </tr>
                     </thead>
                     <tbody>
                           @for ($i=0; $i <= 4; $i++)
                           <tr>
-                            <td><input type="number" class="form-control" id="cantidad_{{$i}}" value="" name="detalle[{{ $i }}][cantidad]" ></td>
-                            <td><select class="form-control" name="detalle[{{ $i }}][host_id]" id="host_{{$i}}" onclick="setCantidad{{$i}}()">
-                              <option value=""></option>
+                            <td><input type="number" class="form-control" id="cantidad_{{$i}}" value="" name="detalle[{{ $i }}][cantidad]" disabled></td>
+                            <td>
+                              <select class="form-control" name="detalle[{{ $i }}][host_id]" id="host_{{$i}}" onclick="setCantidad{{$i}}()">
+                                <option value="">- - - Seleccione - - -</option>
                                 @foreach ($hosts as $host)
                                   <option value="{{$host->id}}"  >{{$host->id}}  |  {{$host->modelo->name}}  |  {{$host->modelo->marca}}  |  {{$host->serial}}</option>
                                 @endforeach
-                              </select></td>
-                            <td><input type="text"  class="form-control" id="obs_01" placeholder="" name="detalle[{{ $i }}][obs]" ></td>
+                              </select>
+                            </td>
+                            <td><input type="text"  class="form-control" id="obs_{{$i}}" placeholder="" name="detalle[{{ $i }}][obs]" disabled></td>
                           </tr>
                            @endfor
                     </tbody>
@@ -60,40 +63,60 @@
     function setCantidad0() {
       if (document.getElementById("host_0").value === "") {
         document.getElementById("cantidad_0").value = "";
+        document.getElementById("cantidad_0").disabled = true;
+        document.getElementById("obs_0").disabled = true;
       }else {
         document.getElementById("cantidad_0").value = 1;
+        document.getElementById("cantidad_0").disabled = false;
+        document.getElementById("obs_0").disabled = false;
       }
     }
 
     function setCantidad1() {
       if (document.getElementById("host_1").value === "") {
         document.getElementById("cantidad_1").value = "";
+        document.getElementById("cantidad_1").disabled = true;
+        document.getElementById("obs_1").disabled = true;
       }else {
         document.getElementById("cantidad_1").value = 1;
+        document.getElementById("cantidad_1").disabled = false;
+        document.getElementById("obs_1").disabled = false;
       }
     }
 
     function setCantidad2() {
       if (document.getElementById("host_2").value === "") {
         document.getElementById("cantidad_2").value = "";
+        document.getElementById("cantidad_2").disabled = true;
+        document.getElementById("obs_2").disabled = true;
       }else {
         document.getElementById("cantidad_2").value = 1;
+        document.getElementById("cantidad_2").disabled = false;
+        document.getElementById("obs_2").disabled = false;
       }
     }
 
     function setCantidad3() {
       if (document.getElementById("host_3").value === "") {
         document.getElementById("cantidad_3").value = "";
+        document.getElementById("cantidad_3").disabled = true;
+        document.getElementById("obs_3").disabled = true;
       }else {
         document.getElementById("cantidad_3").value = 1;
+        document.getElementById("cantidad_3").disabled = false;
+        document.getElementById("obs_3").disabled = false;
       }
     }
 
     function setCantidad4() {
       if (document.getElementById("host_4").value === "") {
         document.getElementById("cantidad_4").value = "";
+        document.getElementById("cantidad_4").disabled = true;
+        document.getElementById("obs_4").disabled = true;
       }else {
         document.getElementById("cantidad_4").value = 1;
+        document.getElementById("cantidad_4").disabled = false;
+        document.getElementById("obs_4").disabled = false;
       }
     }
 </script>
