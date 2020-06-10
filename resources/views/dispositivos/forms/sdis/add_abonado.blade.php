7a598a2d01709cf21202122c1b1cf51cb912555a
Gonzalo <gon.pineiro@gmail.com>
2020-06-10 19:57:06 -0300

    Abonados

--- resources/views/dispositivos/forms/sdis/add_abonado.blade.php
+++ resources/views/dispositivos/forms/sdis/add_abonado.blade.php
@@ -1,96 +1,96 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container">
-
-    <div class="row justify-content-md-center">
-      <div class="col-md-9">
-        <div class="card">
-          <div class="card-header">Agregar Abonado</div>
-            <div class="card-body">
-                <form action="/add_abonado" method="post" name="form">
-                  {{ csrf_field() }}
-                  <div class="form-row">
-                    <div class="form-group col-md-2">
-                      <label for="type">Número</label>
-                      <input type="text" class="form-control" id="numero" name="numero" onkeyup="javascript:this.value=this.value.toUpperCase();" required>
-                    </div>
-                    <div class="form-group col-md-4">
-                      <label for="departament_id">Departamento/Cliente</label>
-                      <select class="form-control" name="departament_id" >
-                        <option value="">- - - Seleccione - - -</option>
-                        @foreach ($departaments as $departament)
-                          <option value="{{$departament->id}}">D: {{$departament->name}} - C: {{$departament->cliente->name}}</option>
-                        @endforeach
-                      </select>
-                    </div>
-                    <div class="form-group col-md-3">
-                      <label for="cliente_id">Plataforma</label> <a href="/plataformas" target= _blank>+</a>
-                      <select class="form-control" name="plataforma_id" >
-                        <option value="">- - - Seleccione - - -</option>
-                        @foreach ($plataformas as $plataforma)
-                          <option value="{{$plataforma->id}}">{{$plataforma->name}}</option>
-                        @endforeach
-                      </select>
-                    </div>
-                    <div class="form-group col-md-3">
-                      <label for="cliente_id">Tipo de abonado</label> <a href="/abonado_types" target= _blank>+</a>
-                      <select class="form-control" name="abonadotype_id" >
-                        <option value="">- - - Seleccione - - -</option>
-                        @foreach ($abonado_types as $abonado_type)
-                          <option value="{{$abonado_type->id}}">{{$abonado_type->name}}</option>
-                        @endforeach
-                      </select>
-                    </div>
-                    <div class="form-group col-md-4">
-                      <label for="email">Email</label>
-                      <input type="text" class="form-control" id="email" name="email" required>
-                    </div>
-                    <div class="form-group col-md-4">
-                      <label for="direccion">Direccion</label>
-                      <input type="text"class="form-control" id="direccion" name="direccion" required>
-                    </div>
-                    <div class="form-group col-md-2">
-                      <label for="localidad">Localidad</label>
-                      <input type="text" class="form-control" id="localidad" name="localidad" required>
-                    </div>
-                    <div class="form-group col-md-2">
-                      <label for="localidad">Teléfono</label>
-                      <input type="number" min="10000" class="form-control" id="localidad" name="telefono" required>
-                    </div>
-                  </div>
-                  <div class="form-row">
-                  </div>
-                  <div class="form-row">
-                    <div class="form-group col-md-3">
-                      <label for="partido">Partido</label>
-                      <input type="text" class="form-control" id="partido" name="partido" required>
-                    </div>
-                    <div class="form-group col-md-3">
-                      <label for="provincia">Provincia</label>
-                      <input type="text"class="form-control" id="provincia" name="provincia" required>
-                    </div>
-                    <div class="form-group col-md-3">
-                      <label for="cp">Código Postal</label>
-                      <input type="number" min = 1 class="form-control" id="cp" name="cp" required>
-                    </div>
-                    <div class="form-group col-md-3">
-                      <label for="partido">Palabra Clave</label>
-                      <input type="text" class="form-control" id="partido" name="palabra_clave" onkeyup="javascript:this.value=this.value.toUpperCase();" required>
-                    </div>
-                  </div>
-
-                  <div class="form-row">
-                    <div class="form-group col-md">
-                      <label for="comentario">Observaciónes</label>
-                      <textarea rows="10" cols="50" type="text" min="10" class="form-control" id="comentario" placeholder="" name="comentario" > </textarea>
-                    </div>
-                  </div>
-                  <button type="submit" class="btn btn-dark">Agregar</button>
-                </form>
+<div class="container">
+  <div class="row justify-content-md-center">
+    <div class="col-md-9">
+      <div class="card">
+        <div class="card-header">Agregar Abonado</div>
+        <div class="card-body">
+          <form action="/add_abonado" method="post" name="form">
+            {{ csrf_field() }}
+            <div class="form-row">
+              <div class="form-group col-md-2">
+                <label for="type">Número</label>
+                <input type="text" class="form-control uppercase" name="numero" required>
               </div>
+              <div class="form-group col-md-4">
+                <label for="departament_id">Departamento/Cliente </label> <a href="/departaments" target=_blank> +</a>
+                <select class="form-control" name="departament_id" required>
+                  <option value="">- - - Seleccione - - -</option>
+                  @foreach ($departaments as $departament)
+                  <option value="{{$departament->id}}">D: {{$departament->name}} - C: {{$departament->cliente->name}}
+                  </option>
+                  @endforeach
+                </select>
+              </div>
+              <div class="form-group col-md-3">
+                <label for="cliente_id">Plataforma</label> <a href="/plataformas" target=_blank>+</a>
+                <select class="form-control" name="plataforma_id" required>
+                  <option value="">- - - Seleccione - - -</option>
+                  @foreach ($plataformas as $plataforma)
+                  <option value="{{$plataforma->id}}">{{$plataforma->name}}</option>
+                  @endforeach
+                </select>
+              </div>
+              <div class="form-group col-md-3">
+                <label for="cliente_id">Tipo de abonado</label> <a href="/abonado_types" target=_blank>+</a>
+                <select class="form-control" name="abonadotype_id" required>
+                  <option value="">- - - Seleccione - - -</option>
+                  @foreach ($abonado_types as $abonado_type)
+                  <option value="{{$abonado_type->id}}">{{$abonado_type->name}}</option>
+                  @endforeach
+                </select>
+              </div>
+              <div class="form-group col-md-4">
+                <label for="email">Email</label>
+                <input type="text" class="form-control" name="email" required>
+              </div>
+              <div class="form-group col-md-4">
+                <label for="direccion">Direccion</label>
+                <input type="text" class="form-control" name="direccion" required>
+              </div>
+              <div class="form-group col-md-2">
+                <label for="localidad">Localidad</label>
+                <input type="text" class="form-control" name="localidad" required>
+              </div>
+              <div class="form-group col-md-2">
+                <label for="localidad">Teléfono</label>
+                <input type="number" min="10000" class="form-control" name="telefono" required>
+              </div>
+            </div>
+
+            <div class="form-row">
+              <div class="form-group col-md-3">
+                <label for="partido">Partido</label>
+                <input type="text" class="form-control" name="partido" required>
+              </div>
+              <div class="form-group col-md-3">
+                <label for="provincia">Provincia</label>
+                <input type="text" class="form-control" name="provincia" required>
+              </div>
+              <div class="form-group col-md-3">
+                <label for="cp">Código Postal</label>
+                <input type="number" min=1 class="form-control" name="cp" required>
+              </div>
+              <div class="form-group col-md-3">
+                <label for="partido">Palabra Clave</label>
+                <input type="text" class="form-control uppercase" name="palabra_clave" required>
+              </div>
+            </div>
+
+            <div class="form-row">
+              <div class="form-group col-md">
+                <label for="comentario">Observaciónes</label>
+                <textarea rows="10" cols="50" type="text" min="10" class="form-control" name="comentario"> </textarea>
+              </div>
+            </div>
+
+            <button type="submit" class="btn btn-dark">Agregar</button>
+          </form>
         </div>
       </div>
+    </div>
   </div>
-
+</div>
 @endsection
--- resources/views/dispositivos/onlys/sdis/abonado.blade.php
+++ resources/views/dispositivos/onlys/sdis/abonado.blade.php
@@ -1,120 +1,128 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container">
-    <div class="row justify-content-md-center">
-      <div class="col-md-9">
-        <div class="card">
-          <div class="card-header">{{$abonado->numero}}</div>
-            <div class="card-body">
-                <form action="/edit_abonado/{{$abonado->id}}" method="get" name="form-edit">
-                  <div class="form-row">
-                      <div class="form-group col-md-2">
-                          <label for="name">Numero</label>
-                          <input type="text" class="form-control" id="numero" placeholder={{$abonado->numero}} disabled>
-                      </div>
-                      <div class="form-group col-md-4">
-                          <label for="modelo">Departamento/Cliente</label>
-                          <input type="text" class="form-control" id="cliente" placeholder={{$abonado->departament->name}} / {{$abonado->departament->cliente->name}} disabled>
-                      </div>
-                      <div class="form-group col-md-3">
-                          <label for="serial">Plataforma</label>
-                          <input type="text" class="form-control" id="type" placeholder={{$abonado->plataforma->name}} disabled>
-                      </div>
-                      <div class="form-group col-md-3">
-                          <label for="serial">Tipo de abonado</label>
-                          <input type="text" class="form-control" id="type" placeholder={{$abonado->abonadotype->name}} disabled>
-                      </div>
-                    </div>
-                    <div class="form-row">
-                      <div class="form-group col-md-4">
-                        <label for="cctv">Email</label>
-                        <input type="text" class="form-control" id="cctv" placeholder="{{$abonado->email}}"  name="email" disabled>
-                      </div>
-                      <div class="form-group col-md-4">
-                        <label for="cctv">Direccion</label>
-                        <input type="text" class="form-control" id="cctv" placeholder="{{$abonado->direccion}}"  name="zona" disabled>
-                      </div>
-                      <div class="form-group col-md-2">
-                        <label for="cctv">Localidad</label>
-                        <input type="text" class="form-control" id="cctv" placeholder="{{$abonado->localidad}}"  name="zona" disabled>
-                      </div>
-                      <div class="form-group col-md-2">
-                        <label for="cctv">Teléfono</label>
-                        <input type="text" class="form-control" id="telefono" placeholder="{{$abonado->telefono}}"  name="telefono" disabled>
-                      </div>
-                    </div>
+<div class="container">
+  <div class="row justify-content-md-center">
+    <div class="col-md-9">
+      <div class="card">
+        <div class="card-header">{{$abonado->numero}}</div>
+        <div class="card-body">
+          <form action="/edit_abonado/{{$abonado->id}}" method="get" name="form-edit">
+            <div class="form-row">
+              <div class="form-group col-md-2">
+                <label for="name">Numero</label>
+                <input type="text" class="form-control" value="{{$abonado->numero}}" readonly>
+              </div>
+              <div class="form-group col-md-4">
+                <label for="modelo">Departamento/Cliente</label>
+                <input type="text" class="form-control"
+                  value="{{$abonado->departament->name}} - C: {{$abonado->departament->cliente->name}}" readonly>
+              </div>
+              <div class="form-group col-md-3">
+                <label for="serial">Plataforma</label>
+                <input type="text" class="form-control" value="{{$abonado->plataforma->name}}" readonly>
+              </div>
+              <div class="form-group col-md-3">
+                <label for="serial">Tipo de abonado</label>
+                <input type="text" class="form-control" value="{{$abonado->abonadotype->name}}" readonly>
+              </div>
+            </div>
 
-                    <div class="form-row">
-                      <div class="form-group col-md-3">
-                        <label for="cctv">Partido</label>
-                        <input type="text" class="form-control" id="cctv" placeholder="{{$abonado->partido}}" name="zona" disabled>
-                      </div>
-                      <div class="form-group col-md-3">
-                        <label for="cctv">Povincia</label>
-                        <input type="text" class="form-control" id="cctv" placeholder="{{$abonado->provincia}}"  name="zona" disabled>
-                      </div>
-                      <div class="form-group col-md-3">
-                        <label for="cctv">Código Postal</label>
-                        <input type="text" class="form-control" id="cctv" placeholder="{{$abonado->cp}}"  name="zona" disabled>
-                      </div>
-                      <div class="form-group col-md-3">
-                        <label for="palabra_clave">Palabra clave</label>
-                        <input type="text" class="form-control" id="cctv" placeholder="{{$abonado->palabra_clave}}" name="palabra_clave" disabled>
-                      </div>
-                    </div>
+            <div class="form-row">
+              <div class="form-group col-md-4">
+                <label for="cctv">Email</label>
+                <input type="text" class="form-control" value="{{$abonado->email}}" name="email" readonly>
+              </div>
+              <div class="form-group col-md-4">
+                <label for="cctv">Direccion</label>
+                <input type="text" class="form-control" value="{{$abonado->direccion}}" name="zona" readonly>
+              </div>
+              <div class="form-group col-md-2">
+                <label for="cctv">Localidad</label>
+                <input type="text" class="form-control" value="{{$abonado->localidad}}" name="zona" readonly>
+              </div>
+              <div class="form-group col-md-2">
+                <label for="cctv">Teléfono</label>
+                <input type="text" class="form-control" value="{{$abonado->telefono}}" name="telefono" readonly>
+              </div>
+            </div>
 
-                    <div class="form-row">
-                      <div class="form-group col-md">
-                        <label for="comentario">Observaciónes</label>
-                        <textarea rows="10" cols="50" type="text" class="form-control" id="comentario" disabled >{{$abonado->comentario}} </textarea>
-                      </div>
-                    </div>
-                    @can ('abonados.edit') <button class="btn btn-dark">Modificar</button> @endcan
-                </form>
-             </div>
-           </div>
+            <div class="form-row">
+              <div class="form-group col-md-3">
+                <label for="cctv">Partido</label>
+                <input type="text" class="form-control" value="{{$abonado->partido}}" name="zona" readonly>
+              </div>
+              <div class="form-group col-md-3">
+                <label for="cctv">Povincia</label>
+                <input type="text" class="form-control" value="{{$abonado->provincia}}" name="zona" readonly>
+              </div>
+              <div class="form-group col-md-3">
+                <label for="cctv">Código Postal</label>
+                <input type="text" class="form-control" value="{{$abonado->cp}}" name="zona" readonly>
+              </div>
+              <div class="form-group col-md-3">
+                <label for="palabra_clave">Palabra clave</label>
+                <input type="text" class="form-control" value="{{$abonado->palabra_clave}}" name="palabra_clave"
+                  readonly>
+              </div>
+            </div>
 
-        @can ('abonados.show')
-        <div class="card">
-         <div class="card-header">Contraseñas/Particiones <a href="/passwords_abonado/{{$abonado->id}}" target= _blank> + </a> </div>
-           <div class="card-body">
-             <div class="col cl-6">
-                 <table class="table table-hover" id="host-table">
-                   <thead>
-                     <tr>
-                       <th scope="col">#</th>
-                       <th scope="col">Particion</th>
-                       <th scope="col">Contraseña</th>
-                       <th scope="col">Editar</th>
-                     </tr>
-                   </thead>
-                   <tbody>
-                     @foreach ($password_abonados as $password_abonado)
-                         <tr>
-                           <td>{{$password_abonado->id}}</td>
-                           <td>{{$password_abonado->particion}}</td>
-                           <td>{{$password_abonado->password}}</td>
-                           @can ('abonados.edit') <td><a href="/edit_password_abonado/{{$password_abonado->id}}" ><img src={{asset("logos/edit-logo.png")}} style="width: 17px;"></a></td> @endcan
-                         </tr>
-                   @endforeach
-                   </tbody>
-                 </table>
+            <div class="form-row">
+              <div class="form-group col-md">
+                <label for="comentario">Observaciónes</label>
+                <textarea rows="10" cols="50" type="text" class="form-control"
+                  readonly>{{$abonado->comentario}} </textarea>
+              </div>
+            </div>
 
-             </div>
-           </div>
-           <script >
-                   $(document).ready(function() {
+            @can ('abonados.edit') <button class="btn btn-dark">Modificar</button> @endcan
+          </form>
+        </div>
+      </div>
+      <br>
+      @can ('abonados.show')
+      <div class="card">
+        <div class="card-header">Contraseñas/Particiones
+          <a href="/passwords_abonado/{{$abonado->id}}" target=_blank> +</a>
+        </div>
+        <div class="card-body">
+          <div class="col cl-6">
+            <table class="table table-hover" id="host-table">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Particion</th>
+                  <th scope="col">Contraseña</th>
+                  <th scope="col">Editar</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($password_abonados as $password_abonado)
+                <tr>
+                  <td>{{$password_abonado->id}}</td>
+                  <td>{{$password_abonado->particion}}</td>
+                  <td>{{$password_abonado->password}}</td>
+                  @can ('abonados.edit')
+                  <td><a href="/edit_password_abonado/{{$password_abonado->id}}"><img
+                        src={{asset("logos/edit-logo.png")}} class="accion_logo"></a>
+                  </td>
+                  @endcan
+                </tr>
+                @endforeach
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <script>
+          $(document).ready(function() {
                    $('#host-table').DataTable({
                      "order": [[ 0, "desc" ]]
                    });
                      } );
-           </script>
-         </div>
-         <br/>
-         @endcan
-
+        </script>
       </div>
-
-
+      @endcan
+    </div>
+  </div>
+</div>
 @endsection
