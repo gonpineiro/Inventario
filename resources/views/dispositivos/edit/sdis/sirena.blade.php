6b68b2020ba453ea1eb74a7eaab16affd2f9c4fe
Gonzalo <gon.pineiro@gmail.com>
2020-06-10 20:52:22 -0300

    Sirenas

--- resources/views/dispositivos/edit/sdis/sirena.blade.php
+++ resources/views/dispositivos/edit/sdis/sirena.blade.php
@@ -1,66 +1,70 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container">
-
-    <div class="row justify-content-md-center">
-      <div class="col-md-9">
-        <div class="card">
-          <div class="card-header">Modificando {{$host->name}}</div>
-            <div class="card-body">
-                <form action="/update_sirena/{{$host->id}}" method="post" name="form">
-                  {{ csrf_field() }}
-                  <div class="form-row">
-                    <div class="form-group col-md-4">
-                      <label for="name">Nombre</label>
-                      <input type="text" class="form-control" id="name" name="name" value="{{$host->name}}" required>
-                    </div>
-                    <div class="form-group col-md-4">
-                      <label for="modelo_id">Modelo</label>
-                        <select class="form-control" name="modelo_id">
-                          <option value="{{$host->modelo_id}}">{{$host->modelo->name}}</option>
-                          @foreach ($modelos as $modelo)
-                            <option value="{{$modelo->id}}">{{$modelo->name}}</option>
-                          @endforeach
-                        </select>
-                    </div>
-                    <div class="form-group col-md-4">
-                      <label for="serial">Serial</label>
-                      <input type="text" class="form-control" id="serial" value="{{$host->serial}}" name="serial" style="text-transform:uppercase;" onkeyup="javascript:this.value=this.value.toUpperCase();" required>
-                    </div>
-                  </div>
-                  <div class="form-row">
-                        <div class="form-group col-md-5">
-                          <label for="abonado_id">Conectado</label>
-                            <select class="form-control" name="cctv_id">
-                              @if ($host->host->host_type_id == 40) <option value="{{$host->cctv_id}}">P: {{$host->host->id}} - AB: {{$host->host->abonado->numero}} - C: {{$host->host->abonado->departament->cliente->name}}</option> @endif
-
-                              <option disabled>Paneles</option>
-                              @foreach ($panel_alarms as $panel_alarm)
-                                <option value={{$panel_alarm->id}}>P: {{$panel_alarm->id}} - AB: {{$panel_alarm->abonado->id}} - C: {{$panel_alarm->abonado->departament->cliente->name}}</option>
-                              @endforeach
-                            </select>
-                        </div>
-                        <div class="form-group col-md-5">
-                          <label for="valor">Ubicación</label>
-                          <input type="text" class="form-control" id="zona" value="{{$host->zona}}" name="zona" required>
-                        </div>
-                        <div class="form-group col-md-2">
-                          <label for="valor">Valor</label>
-                          <input type="number" min="10" class="form-control" id="valor" value="{{$host->valor}}" name="valor" required>
-                        </div>
-                  </div>
-                  <div class="form-row">
-                    <div class="form-group col-md">
-                      <label for="comentario">Observaciónes</label>
-                      <textarea rows="10" cols="50" type="text" class="form-control" id="comentario" name="comentario" >{{$host->comentario}} </textarea>
-                    </div>
-                  </div>
-                  <button type="submit" class="btn btn-dark">Agregar</button>
-                </form>
+<div class="container">
+  <div class="row justify-content-md-center">
+    <div class="col-md-9">
+      <div class="card">
+        <div class="card-header">Modificando {{$host->name}}</div>
+        <div class="card-body">
+          <form action="/update_sirena/{{$host->id}}" method="post" name="form">
+            {{ csrf_field() }}
+            <div class="form-row">
+              <div class="form-group col-md-4">
+                <label for="name">Nombre</label>
+                <input type="text" class="form-control" name="name" value="{{$host->name}}" required>
               </div>
+              <div class="form-group col-md-4">
+                <label for="modelo_id">Modelo</label> <a href="/modelos" target=_blank> +</a>
+                <select class="form-control" name="modelo_id" required>
+                  <option value="{{$host->modelo_id}}">{{$host->modelo->name}}</option>
+                  @foreach ($modelos as $modelo)
+                  <option value="{{$modelo->id}}">{{$modelo->name}}</option>
+                  @endforeach
+                </select>
+              </div>
+              <div class="form-group col-md-4">
+                <label for="serial">Serial</label>
+                <input type="text" class="form-control uppercase" value="{{$host->serial}}" name="serial" required>
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="form-group col-md-5">
+                <label for="abonado_id">Panel/Abonado/Cliente </label><a href="/form_panel_alarm" target=_blank> +</a>
+                <select class="form-control" name="cctv_id">
+                  @if ($host->host->host_type_id == 40) <option value="{{$host->cctv_id}}">P: {{$host->host->id}} - AB:
+                    {{$host->host->abonado->numero}} - C: {{$host->host->abonado->departament->cliente->name}}</option>
+                  @endif
+
+                  <option disabled>Paneles</option>
+                  @foreach ($panel_alarms as $panel_alarm)
+                  <option value={{$panel_alarm->id}}>P: {{$panel_alarm->id}} - AB: {{$panel_alarm->abonado->id}} - C:
+                    {{$panel_alarm->abonado->departament->cliente->name}}</option>
+                  @endforeach
+                </select>
+              </div>
+              <div class="form-group col-md-5">
+                <label for="valor">Ubicación</label>
+                <input type="text" class="form-control" value="{{$host->zona}}" name="zona" required>
+              </div>
+              <div class="form-group col-md-2">
+                <label for="valor">Valor</label>
+                <input type="number" min="10" class="form-control" value="{{$host->valor}}" name="valor" required>
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="form-group col-md">
+                <label for="comentario">Observaciónes</label>
+                <textarea rows="10" cols="50" type="text" class="form-control" id="comentario"
+                  name="comentario">{{$host->comentario}} </textarea>
+              </div>
+            </div>
+            <button type="submit" class="btn btn-dark">Agregar</button>
+          </form>
         </div>
       </div>
+    </div>
   </div>
+</div>
 
 @endsection
--- resources/views/dispositivos/forms/sdis/add_sirena.blade.php
+++ resources/views/dispositivos/forms/sdis/add_sirena.blade.php
@@ -1,65 +1,65 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container">
-
-    <div class="row justify-content-md-center">
-      <div class="col-md-9">
-        <div class="card">
-          <div class="card-header">Agregar Sirena</div>
-            <div class="card-body">
-                <form action="/add_sirena" method="post" name="form">
-                  {{ csrf_field() }}
-                  <div class="form-row">
-                    <div class="form-group col-md-4">
-                      <label for="name">Nombre</label>
-                      <input type="text" class="form-control" id="name" name="name" placeholder="" required>
-                    </div>
-                    <div class="form-group col-md-4">
-                      <label for="modelo_id">Modelo</label>
-                        <select class="form-control" name="modelo_id">
-                          <option value="">- - - Seleccione - - -</option>
-                          @foreach ($modelos as $modelo)
-                            <option value="{{$modelo->id}}">{{$modelo->name}}</option>
-                          @endforeach
-                        </select>
-                    </div>
-                    <div class="form-group col-md-4">
-                      <label for="serial">Serial</label>
-                      <input type="text" class="form-control" id="serial" placeholder="" name="serial" style="text-transform:uppercase;" onkeyup="javascript:this.value=this.value.toUpperCase();" required>
-                    </div>
-                  </div>
-                  <div class="form-row">
-                    <div class="form-group col-md-5">
-                      <label for="abonado_id">Panel/Abonado/Cliente</label>
-                        <select class="form-control" name="cctv_id" required>
-                          <option selected disabled>- - - Seleccione - - -</option>
-                          <option disabled>Paneles</option>
-                          @foreach ($panel_alarms as $panel_alarm)
-                            <option value={{$panel_alarm->id}}>P: {{$panel_alarm->id}} - AB: {{$panel_alarm->abonado->numero}} - C: {{$panel_alarm->abonado->departament->cliente->name}}</option>
-                          @endforeach
-                        </select>
-                    </div>
-                    <div class="form-group col-md-5">
-                      <label for="zona">Ubicación</label>
-                      <input type="text" class="form-control" id="zona" placeholder="" name="zona" required>
-                    </div>
-                    <div class="form-group col-md-2">
-                      <label for="valor">Valor</label>
-                      <input type="number" min="10" class="form-control" id="valor" placeholder="" name="valor" required>
-                    </div>
-                  </div>
-                  <div class="form-row">
-                        <div class="form-group col-md">
-                          <label for="comentario">Observaciónes</label>
-                          <textarea rows="10" cols="50" type="text" min="10" class="form-control" id="comentario" placeholder="" name="comentario" > </textarea>
-                        </div>
-                  </div>
-                  <button type="submit" class="btn btn-dark">Agregar</button>
-                </form>
+<div class="container">
+  <div class="row justify-content-md-center">
+    <div class="col-md-9">
+      <div class="card">
+        <div class="card-header">Agregar Sirena</div>
+        <div class="card-body">
+          <form action="/add_sirena" method="post" name="form">
+            {{ csrf_field() }}
+            <div class="form-row">
+              <div class="form-group col-md-4">
+                <label for="name">Nombre</label>
+                <input type="text" class="form-control" name="name" required>
               </div>
+              <div class="form-group col-md-4">
+                <label for="modelo_id">Modelo </label> <a href="/modelos" target=_blank> +</a>
+                <select class="form-control" name="modelo_id" required>
+                  <option value="">- - - Seleccione - - -</option>
+                  @foreach ($modelos as $modelo)
+                  <option value="{{$modelo->id}}">{{$modelo->name}}</option>
+                  @endforeach
+                </select>
+              </div>
+              <div class="form-group col-md-4">
+                <label for="serial">Serial</label>
+                <input type="text" class="form-control uppercase" name="serial" required>
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="form-group col-md-5">
+                <label for="abonado_id">Panel/Abonado/Cliente </label> <a href="/form_panel_alarm" target=_blank> +</a>
+                <select class="form-control" name="cctv_id" required>
+                  <option selected disabled>- - - Seleccione - - -</option>
+                  <option disabled>Paneles</option>
+                  @foreach ($panel_alarms as $panel_alarm)
+                  <option value={{$panel_alarm->id}}>P: {{$panel_alarm->id}} - AB: {{$panel_alarm->abonado->numero}} -
+                    C: {{$panel_alarm->abonado->departament->cliente->name}}</option>
+                  @endforeach
+                </select>
+              </div>
+              <div class="form-group col-md-5">
+                <label for="zona">Ubicación</label>
+                <input type="text" class="form-control" name="zona" required>
+              </div>
+              <div class="form-group col-md-2">
+                <label for="valor">Valor</label>
+                <input type="number" min="10" class="form-control" name="valor" required>
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="form-group col-md">
+                <label for="comentario">Observaciónes</label>
+                <textarea rows="10" cols="50" type="text" min="10" class="form-control" name="comentario"> </textarea>
+              </div>
+            </div>
+            <button type="submit" class="btn btn-dark">Agregar</button>
+          </form>
         </div>
       </div>
+    </div>
   </div>
-
+</div>
 @endsection
